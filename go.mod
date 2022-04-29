21350bf12a38e25278f7276fe1c6cea8daf93f75
shogohamda <>
2022-04-29 12:50:18 +0900

    wip

---
+++ go.mod
@@ -0,0 +1,37 @@
+module spnPkg
+
+go 1.18
+
+require (
+	cloud.google.com/go v0.100.2 // indirect
+	cloud.google.com/go/compute v1.6.1 // indirect
+	cloud.google.com/go/monitoring v1.4.0 // indirect
+	cloud.google.com/go/spanner v1.30.1 // indirect
+	cloud.google.com/go/trace v1.2.0 // indirect
+	contrib.go.opencensus.io/exporter/stackdriver v0.13.10 // indirect
+	github.com/GoogleCloudPlatform/golang-samples/spanner v0.0.0-20220426210119-fc609838280f // indirect
+	github.com/aws/aws-sdk-go v1.43.27 // indirect
+	github.com/census-instrumentation/opencensus-proto v0.3.0 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/cncf/udpa/go v0.0.0-20220112060539-c52dc94e7fbe // indirect
+	github.com/cncf/xds/go v0.0.0-20220314180256-7f1daf1720fc // indirect
+	github.com/envoyproxy/go-control-plane v0.10.1 // indirect
+	github.com/envoyproxy/protoc-gen-validate v0.6.7 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.7 // indirect
+	github.com/googleapis/gax-go/v2 v2.3.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/net v0.0.0-20220412020605-290c469a71a5 // indirect
+	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/xerrors v0.0.0-20220411194840-2f41105eb62f // indirect
+	google.golang.org/api v0.75.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20220421151946-72621c1f0bd3 // indirect
+	google.golang.org/grpc v1.45.0 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
---
+++ snippet.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"regexp"
+	// "strconv"
+	"time"
+
+	"cloud.google.com/go/spanner"
+	// "google.golang.org/api/iterator"
+
+	database "cloud.google.com/go/spanner/admin/database/apiv1"
+	adminpb "google.golang.org/genproto/googleapis/spanner/admin/database/v1"
+)
+
+type command func(ctx context.Context, w io.Writer, client *spanner.Client) error
+type adminCommand func(ctx context.Context, w io.Writer, adminClient *database.DatabaseAdminClient, database string) error
+
+var (
+	commands = map[string]command{
+		// "write":               write,
+		// "read":                read,
+		// "query":               query,
+		// "update":              update,
+		// "querynewcolumn":      queryNewColumn,
+		// "pgquerynewcolumn":    pgQueryNewColumn,
+		// "querywithparameter":  queryWithParameter,
+		// "pgqueryparameter":    pgQueryParameter,
+		// "dmlwrite":            writeUsingDML,
+		// "pgdmlwrite":          pgWriteUsingDML,
+		// "dmlwritetxn":         writeWithTransactionUsingDML,
+		// "pgdmlwritetxn":       pgWriteWithTransactionUsingDML,
+		// "readindex":           readUsingIndex,
+		// "readstoringindex":    readStoringIndex,
+		// "readonlytransaction": readOnlyTransaction,
+	}
+
+	adminCommands = map[string]adminCommand{
+		"createdatabase":    createDatabase,
+		// "addnewcolumn":      addNewColumn,
+		// "pgaddnewcolumn":    pgAddNewColumn,
+		// "addstoringindex":   addStoringIndex,
+		// "pgaddstoringindex": pgAddStoringIndex,
+		// "pgcreatedatabase":  pgCreateDatabase,
+	}
+)
+
+func createDatabase(ctx context.Context, w io.Writer, adminClient *database.DatabaseAdminClient, db string) error {
+	matches := regexp.MustCompile("^(.*)/databases/(.*)$").FindStringSubmatch(db)
+	if matches == nil || len(matches) != 3 {
+		return fmt.Errorf("Invalid database id %s", db)
+	}
+	op, err := adminClient.CreateDatabase(ctx, &adminpb.CreateDatabaseRequest{
+		Parent:          matches[1],
+		CreateStatement: "CREATE DATABASE `" + matches[2] + "`",
+		ExtraStatements: []string{
+			`CREATE TABLE Singers (
+				SingerId   INT64 NOT NULL,
+				FirstName  STRING(1024),
+				LastName   STRING(1024),
+				SingerInfo BYTES(MAX)
+			) PRIMARY KEY (SingerId)`,
+			`CREATE TABLE Albums (
+				SingerId     INT64 NOT NULL,
+				AlbumId      INT64 NOT NULL,
+				AlbumTitle   STRING(MAX)
+			) PRIMARY KEY (SingerId, AlbumId),
+			INTERLEAVE IN PARENT Singers ON DELETE CASCADE`,
+		},
+	})
+	if err != nil {
+		return err
+	}
+	if _, err := op.Wait(ctx); err != nil {
+		return err
+	}
+	fmt.Fprintf(w, "Created database [%s]\n", db)
+	return nil
+}
+
+func createClients(ctx context.Context, db string) (*database.DatabaseAdminClient, *spanner.Client) {
+	adminClient, err := database.NewDatabaseAdminClient(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dataClient, err := spanner.NewClient(ctx, db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return adminClient, dataClient
+}
+
+func run(ctx context.Context, adminClient *database.DatabaseAdminClient, dataClient *spanner.Client, w io.Writer, cmd string, db string) error {
+	if adminCmdFn := adminCommands[cmd]; adminCmdFn != nil {
+		err := adminCmdFn(ctx, w, adminClient, db)
+		if err != nil {
+			fmt.Fprintf(w, "%s failed with %v", cmd, err)
+		}
+		return err
+	}
+
+	// Normal mode
+	cmdFn := commands[cmd]
+	if cmdFn == nil {
+		flag.Usage()
+		os.Exit(2)
+	}
+	err := cmdFn(ctx, w, dataClient)
+	if err != nil {
+		fmt.Fprintf(w, "%s failed with %v", cmd, err)
+	}
+	return err
+}
+
+
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `Usage: spanner_snippets <command> <database_name>
+	Command can be one of: write, read, query, update, querynewcolumn,
+		querywithparameter, dmlwrite, dmlwritetxn, readindex, readstoringindex,
+		readonlytransaction, createdatabase, addnewcolumn, addstoringindex,
+		pgcreatedatabase, pgqueryparameter, pgdmlwrite, pgaddnewcolumn, pgquerynewcolumn,
+		pgdmlwritetxn, pgaddstoringindex
+Examples:
+	spanner_snippets createdatabase projects/my-project/instances/my-instance/databases/example-db
+	spanner_snippets write projects/my-project/instances/my-instance/databases/example-db
+`)
+	}
+
+	flag.Parse()
+	if len(flag.Args()) < 2 {
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	cmd, db := flag.Arg(0), flag.Arg(1)
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
+	defer cancel()
+	adminClient, dataClient := createClients(ctx, db)
+	defer adminClient.Close()
+	defer dataClient.Close()
+	if err := run(ctx, adminClient, dataClient, os.Stdout, cmd, db); err != nil {
+		os.Exit(1)
+	}
+}
